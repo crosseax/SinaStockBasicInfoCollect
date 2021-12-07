34a7ea502387abf298a94d15aaffe57aed257b2b
crosseax <qq417737572@gmail.com>
2021-12-07 19:37:50 +0800

    renaming variables

--- test/main_test.py
+++ test/main_test.py
@@ -16,12 +16,12 @@ def main():
     allCompanies = []
 
     # generating URLs lists
-    allSinaURLs = u.xueqiu_url_generator(beginNum, endNum)
+    allXueqiuUrls = u.xueqiu_url_generator(beginNum, endNum)
 
     # multi-threading executing the web scrapping function
     with concurrent.futures.ThreadPoolExecutor () as executor:
         print("Running...")
-        for i in executor.map(u.xueqiu_web_info_getter, allSinaURLs):
+        for i in executor.map(u.xueqiu_web_info_getter, allXueqiuUrls):
             try:
                 if i:
                     allCompanies.append(i)
